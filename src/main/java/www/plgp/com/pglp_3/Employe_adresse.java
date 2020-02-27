4558d813202ddb9270208a0a2efef5ab137e1672
Only_soft <abdullahwin99@gmail.com>
2020-02-27 12:08:50 +0100

    construction de la classe Employe_adresse

---
+++ src/main/java/www/plgp/com/pglp_3/Employe_adresse.java
@@ -0,0 +1,14 @@
+package www.plgp.com.pglp_3;
+
+public class Employe_adresse 
+{
+ private  String nom;
+ private  String adresse;
+ public void afficheCoordonnees()
+ {
+	 
+	 System.out.println(nom+", "+adresse);
+ }
+ 
+ 
+}
